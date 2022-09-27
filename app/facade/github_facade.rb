22c45da54a744442d4ba0b681c4b95b19e4040c5
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-09-26 21:47:20 -0600

    add beginning api work

--- app/facade/github_facade.rb
+++ app/facade/github_facade.rb
@@ -1,50 +1,11 @@
-# require "./app/service/github_service"
-# require "./app/poros/github_repo"
+require "./app/service/github_service"
+require "./app/poros/holidays"
 
-# class GitHubFacade
-#   # def self.repo_name
-#   #   data = GitHubService.get_repos
-#   #   data.map do |repo_data|
-#   #     GitHubRepo.new(repo_data)
-#   #   end
-#   # end
-  # def self.user_names
-  #   response = GitHubService.request("collaborators", true)
-  #   response.map { |user| user[:login] }.sort
-  # end
-
-#   def self.all_repos
-#     data = GitHubService.get_repos
-
-#     data.map do |data|
-#       GitHubRepo.new(data)
-#     end
-#   end
-
-#   def self.repo_name
-#     all_repos.map do |data|
-#       if data.name.include?("little-esty-shop")
-#         return data.full_name
-#       end
-#     end
-
-#     nil
-#   end
-
-  # def self.all_commits
-  #   data = GitHubService.get_commits
-  #   data[0][:commit][:author][:name]
-  #   authors_commits = Hash.new{ |author, commit_num| author[commit_num] = 0}
-  #   data.each do |commit|
-  #     commit = data[:commit]
-  #     author = data[:commit][:author][:name]
-  #   end
-  # end
-
-#   def self.pull_requests
-#     pr_data = GitHubService.get_pull_requests
-
-#     #pr_data.map { |data| data[:number] }
-#     pr_data[0][:number]
-#   end
-# end
+class GitHubFacade
+  def self.holidays
+    holiday_data = GitHubService.get_holidays
+    holiday_data.map do |holiday_data|
+      Holiday.new(holiday_data)
+    end
+  end
+end
--- app/service/github_service.rb
+++ app/service/github_service.rb
@@ -1,44 +1,14 @@
-# require "httparty"
-# require "json"
+require "httparty"
+require "json"
 
-# class GitHubService
-#   def self.get_repos
-#     get_uri("https://api.github.com/users/sjmann2/repos")
-#   end
+class GitHubService
+  def self.get_repos
+    get_uri("https://date.nager.at/api/v3/NextPublicHolidays/us")
+  end
 
-#   def self.get_pull_requests
-#     get_pr("https://api.github.com/repos/sjmann2/little-esty-shop/pulls?state=all")
-#   end
-
-#   # def self.get_commits
-#   #   page1 = get_url_commits("https://api.github.com/repos/sjmann2/little-esty-shop/commits?q=addClass+user:mozilla&per_page=100&page=1")
-#   #   page2 = get_url_commits("https://api.github.com/repos/sjmann2/little-esty-shop/commits?q=addClass+user:mozilla&per_page=100&page=2")
-#   #   page3 = get_url_commits("https://api.github.com/repos/sjmann2/little-esty-shop/commits?q=addClass+user:mozilla&per_page=100&page=3")
-#   # end
-
-#   def self.get_uri(uri)
-#     return [{name: "little-esty-shop", full_name: "sjmann2/little-esty-shop"}] if Rails.env == "test"
-#     response = HTTParty.get(uri,     headers: {"Authorization" => "Bearer " + ENV["TOKEN"]})
-
-#     # ENV.fetch('TOKEN')
-#     JSON.parse(response.body,     symbolize_names: true)
-#   end
-
-#   def self.request(path, auth_required = false)
-#     return [{login: "noahvanekdom"}] if Rails.env == "test"
-#     response = HTTParty.get("https://api.github.com/repos/sjmann2/little-esty-shop/#{path}",     headers: {authorization: "Bearer " + ENV["UNAMETOKEN"]})
-#     JSON.parse(response.body,     symbolize_names: true)
-#   end
-
-#   def self.get_pr(uri)
-#     return [{number: 37}] if Rails.env == "test"
-#     response = HTTParty.get(uri)
-#     JSON.parse(response.body,     symbolize_names: true)
-#   end
-
-#   # def self.get_url_commits(url)
-#   #   return [{number_commits: 279}] if Rails.env == 'test'
-#   #   response = HTTParty.get(url) 
-#   #   parsed = JSON.parse(response.body, symbolize_names: true)
-#   # end
-# end
+  def self.get_uri(uri)
+    return [{date: '10/31/2022', name: "Halloween"}, {date: '12/25/2022', name: "Thanksgiving"}, {date: '12/25/2023', name: "Christmas"}] if Rails.env == 'test'    response = HTTParty.get(uri)
+    data = HTTParty.get(uri)
+    parsed = JSON.parse(data.body, symbolize_names: true)
+    end
+end
